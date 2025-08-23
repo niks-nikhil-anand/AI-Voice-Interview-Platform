775be63f29dcf64c8fbf817f0b63d6a59624e1f3
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-08-23 18:04:54 +0530

    Added constants , assets , types etc.

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const HomeLayout = ({children} : {children:ReactNode}) => {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default HomeLayout
