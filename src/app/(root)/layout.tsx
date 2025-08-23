4965674b87eb02769b938d92b6ab561b314f1a66
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-08-24 02:29:00 +0530

    Done with the 2 section , and InterviewCard

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,19 @@
-import React, { ReactNode } from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React, { ReactNode } from "react";
 
-const HomeLayout = ({children} : {children:ReactNode}) => {
+const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-        {children}
+    <div className="root-layout">
+      <nav>
+        <Link href={"/"} className="flex items-center gap-2">
+          <Image src={"/logo.svg"} height={32} width={38} alt="Logo" />
+        <h2 className="text-primary-100">Prepwise</h2>
+        </Link>
+      </nav>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;
