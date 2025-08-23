49d0537da64aa58b6ff671260ae03430bccf7b3b
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-08-24 00:12:33 +0530

    Done with the Auth Pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${geistSans.className} antialiased`}>{children}</body>
+      <body className={`${geistSans.className} antialiased pattern`}>
+        {children}
+        <Toaster/>
+      </body>
     </html>
   );
 }
